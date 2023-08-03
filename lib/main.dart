054bb2499fe9d114bca9081f5f1474d5085afe43
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-02 20:02:10 -0500

    fix(home): move to main

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,19 @@ import 'ui/screens/home_app.dart';
 void main() {
   runApp(const Home());
 }
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeApp(),
+    );
+  }
+}
--- lib/ui/screens/home_app.dart
+++ lib/ui/screens/home_app.dart
@@ -2,22 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../interface/home_app_interface.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HomeApp(),
-    );
-  }
-}
-
 class HomeApp extends StatefulWidget {
   const HomeApp({super.key});
 
