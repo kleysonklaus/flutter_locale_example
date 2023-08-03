044ac47625509b3c34071881d9bd7bb3d351828d
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-02 20:01:34 -0500

    feat(main): create a screen

---
+++ lib/ui/screens/home_app.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../interface/home_app_interface.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> implements HomeAppInterface {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(
+            "Example Locale Languaje",
+          ),
+        ),
+        body: Text(
+          "asd",
+        ),
+      ),
+    );
+  }
+
+  @override
+  void showLoading() {
+    // TODO: implement showLoading
+  }
+}
