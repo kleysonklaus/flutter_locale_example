3979ee8519c4db3089c74e1316377bf55ac994a7
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-02 23:46:25 -0500

    feat(home_app): get locales when changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,41 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'localizations/app_home_localizations.dart';
 import 'ui/screens/scafold_screen.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final String defaultLocale = Platform.localeName;
+  print(defaultLocale);
+
+  final List<Locale> systemLocales = WidgetsBinding.instance.window.locales;
+
+  print(systemLocales);
+
+  final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  String deviceLanguage = '';
+  try {
+    if (Platform.isAndroid) {
+      // Configuración para Android
+      final AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
+      print(androidInfo.device);
+      // deviceLanguage = androidInfo.device.deviceLocale.languageCode;
+    } else if (Platform.isIOS) {
+      // Configuración para iOS
+      deviceLanguage = 'en'; // Establece inglés por defecto
+    }
+  } on PlatformException {
+    deviceLanguage = "en"; // Establece inglés por defecto
+  }
+
+  print("final: $deviceLanguage");
+
   runApp(const Home());
 }
 
--- lib/ui/screens/home_app.dart
+++ lib/ui/screens/home_app.dart
@@ -15,15 +15,40 @@ class HomeApp extends StatefulWidget {
   State<HomeApp> createState() => _HomeAppState();
 }
 
-class _HomeAppState extends State<HomeApp> implements HomeAppInterface {
+class _HomeAppState extends State<HomeApp>
+    with WidgetsBindingObserver
+    implements HomeAppInterface {
   late HomeAppPresenter presenter;
   late HomeAppModel modelUi;
+
+  // String currentDefaultSystemLocale;
+  // List<Locale> currentSystemLocales;
+
+  // Here we read the current locale values
+  // void setCurrentValues() {
+  //   currentSystemLocales = WidgetsBinding.instance.window.locales;
+  //   currentDefaultSystemLocale = Platform.localeName;
+  // }
 
   @override
   void initState() {
     presenter = HomeAppPresenter(this);
     presenter.getModelUi(widget.languaje);
+    WidgetsBinding.instance.addObserver(this);
+
     super.initState();
+  }
+
+  @override
+  void didChangeLocales(List<Locale>? locale) {
+    print("SE CAMBIAAAAAAAAAAAAAAAA");
+    print(locale);
+    // This is run when system locales are changed
+    super.didChangeLocales(locale);
+    // Update state with the new values and redraw controls
+    setState(() {
+      // setCurrentValues();
+    });
   }
 
   static const TextStyle style = TextStyle(
